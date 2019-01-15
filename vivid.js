eff91948607a5cd1fed97b139f823a3b74d5c663
bleonard252 <bleonard252@rocketmail.com>
2019-01-14 20:25:13 -0500

    done cannot be done
    Signed-off-by: bleonard252 <bleonard252@rocketmail.com>

--- vivid.js
+++ vivid.js
@@ -44,7 +44,7 @@ var v = {
                     document.getElementById(posts_id).innerHTML = document.getElementById(posts_id).innerHTML +
                         `<div class="demo-card-square mdl-card mdl-shadow--2dp" id="postcard-${status.id}">
                 <div class="mdl-card__title ttk-card-title" id="postcard-${status.id}-title">
-                <span class="mdl-chip mdl-chip--contact" onClick='window.location.hash = "profile/${status.id}";v.over.show("sub/profile.html").done(vsub.profile)'>
+                <span class="mdl-chip mdl-chip--contact" onClick='window.location.hash = "profile/${status.id}";v.over.show("sub/profile.html").then(vsub.profile)'>
                     <img class="mdl-chip__contact" src="${status.account.avatar}"></img>
                     <span class="mdl-chip__text">${v.profile.name(status.account)}</span>
                 </span>
