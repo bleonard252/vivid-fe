817497e4d9cc1fe2c9d510d36e55d7da80925ebe
bleonard252 <bleonard252@rocketmail.com>
2019-01-14 19:55:25 -0500

    it's not executing (xss?)
    Signed-off-by: bleonard252 <bleonard252@rocketmail.com>

--- vivid.js
+++ vivid.js
@@ -44,7 +44,7 @@ var v = {
                     document.getElementById(posts_id).innerHTML = document.getElementById(posts_id).innerHTML +
                         `<div class="demo-card-square mdl-card mdl-shadow--2dp" id="postcard-${status.id}">
                 <div class="mdl-card__title ttk-card-title" id="postcard-${status.id}-title">
-                <span class="mdl-chip mdl-chip--contact" onClick='window.location.hash = "profile/${status.id}";v.over.show("sub/profile.html")'>
+                <span class="mdl-chip mdl-chip--contact" onClick='window.location.hash = "profile/${status.id}";'>
                     <img class="mdl-chip__contact" src="${status.account.avatar}"></img>
                     <span class="mdl-chip__text">${v.profile.name(status.account)}</span>
                 </span>
@@ -78,7 +78,7 @@ var v = {
                     }
                     if (status.reblog !== null) {if (status.reblogged === false) {
                         document.getElementById("postcard-"+status.id+"-title").innerHTML = 
-                        `<span class="mdl-chip mdl-chip--contact" onClick='window.location.hash = "profile/${status.id}";v.over.show("sub/profile.html")'>
+                        `<span class="mdl-chip mdl-chip--contact" onClick='window.location.hash = "profile/${status.id}";v.over.case.subprofile(api)'>
                         <img class="mdl-chip__contact" src="${status.reblog.account.avatar}"></img>
                         <span class="mdl-chip__text">${v.profile.name(status.reblog.account)}</span>
                         </span>`;
@@ -253,6 +253,16 @@ var v = {
                 if (document.getElementById("v-over") !== null) { return true; } else { console.error("v.over.isShown(): Inconsistent class + element!") }
             }
             else { return false; }
+        },
+        case: {
+            subprofile: async function(api) {
+                await v.over.show("sub/profile.html");
+                let zxhash = window.location.hash;
+                console.log("ZXHASH (URL extension): " + zxhash)
+                zxhash = zxhash.replace("profile\/","");
+                console.log("ZXHASH (profile ID): " + zxhash)
+                v.profile.getAll("PROFILE_SUBPG_CONTENT",zxhash,api)
+            }
         }
     }
 }
